77a28e9935e4be0b9ad04289c13ac66b1725e2bb
Uyai-Abasi <lorddansco@gmain.com>
2023-09-26 11:32:52 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,92 @@
+let inputTap = document.getElementById('input');
+let submitTap = document.getElementById('equal');
+let Tages = document.querySelector('strong');
+let clear = document.querySelector('#reset');
+let formTag = document.querySelector('form');
+let backBtn = document.querySelector('.bg');
+let content = document.querySelector('.content');
+let deleteTag = document.getElementById('delete');
+let button = document.querySelectorAll('button');
+let selection = document.getElementsByClassName('section')[0];
+let body = document.body;
+
+let space = 20;
+Tages.addEventListener('click', function () {
+    Tages.style.left = `${space}px`;
+
+    changeBackground(space)
+
+    space *= 2;
+})
+formTag.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+
+    inputTap.value = eval(inputTap.value)
+})
+function changeBackground(value) {
+    if (value == 20) {
+        changeStyle();
+        
+    } else if (value == 40) {
+        changeStyle2()
+    
+    } else {
+        Tages.style.left = `0px`;
+        body.style.backgroundColor = " hsl(222, 26%, 31%)";
+        backBtn.style.backgroundColor = "yellow "
+        content.style.color = "white"
+        location.reload()
+    }
+
+}
+
+function changeStyle() {
+    selection.style.backgroundColor = " hsl(0, 5%, 81%)";
+    Tages.style.backgroundColor = "hsl(25, 98%, 40%)";
+    inputTap.style.backgroundColor = "white";
+    inputTap.style.color = "black";
+    body.style.backgroundColor = "  hsl(0, 0%, 90%)";
+    backBtn.style.backgroundColor = "hsl(0, 5%, 81%)";
+    submitTap.style.backgroundColor ="hsl(25, 98%, 40%)";
+    submitTap.style.borderColor ="hsl(185, 58%, 25%)";
+    clear.style.backgroundColor = "hsl(185, 42%, 37%)"
+    clear.style.borderBottom =  "3px  soloid hsl(185, 58%, 25%)"
+    deleteTag.style.backgroundColor = "hsl(185, 42%, 37%)";
+    deleteTag.style.borderColor = "hsl(25, 99%, 27%)";
+    content.style.color = "black";
+    clear.style.borderBottom = "thick solid hsl(185, 58%, 25%)";
+    deleteTag.style.borderBottom = "thick solid hsl(185, 58%, 25%)";
+    submitTap.style.borderBottom = "thick solid hsl(25, 99%, 27%)";
+}
+function changeStyle2() {
+    [...button].forEach(item => {
+        item.style.backgroundColor = "hsl(268, 47%, 21%)";
+        item.style.color = "yellow";
+        item.style.borderWidth = " bottom 2px solid red";
+        item.style.borderBottom = "thick solid hsl(290, 70%, 36%)";
+    });
+    selection.style.backgroundColor = " hsl(268, 71%, 12%)";
+    Tages.style.backgroundColor = "hsl(176, 100%, 44%)";
+    body.style.backgroundColor = " hsl(268, 75%, 9%)";
+    backBtn.style.backgroundColor = "hsl(268, 71%, 12%) "
+    content.style.color = "yellow"
+    submitTap.style.backgroundColor ="hsl(176, 100%, 44%)";
+    submitTap.style.color ="white";
+    clear.style.backgroundColor ="hsl(281, 89%, 26%)";
+    clear.style.color ="white";
+    deleteTag.style.backgroundColor = "hsl(281, 89%, 26%)";
+    deleteTag.style.color = "white";
+    inputTap.style.color ="yellow";
+    inputTap.style.backgroundColor ="hsl(268, 71%, 12%)";
+
+
+}
+
+function handleClick(value) {
+    inputTap.value += value;
+}
+
+function clearValue() {
+    inputTap.value = "";
+}
---
+++ style-guide.md
@@ -0,0 +1,93 @@
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+## Colors
+
+### Theme 1
+
+#### Backgrounds
+
+--Very-dark-desaturated-blue: hsl(222, 26%, 31%)
+--Very-dark-desaturated-blue: hsl(223, 31%, 20%)
+--Very-dark-desaturated-blue: hsl(224, 36%, 15%)
+
+#### Keys
+
+--Desaturated-dark-blue: hsl(225, 21%, 49%)
+--Desaturated-dark-blue: hsl(224, 28%, 35%)
+
+- Red: hsl(6, 63%, 50%)
+- Dark-red: hsl(6, 70%, 34%)
+
+--Light-grayish-orange: hsl(30, 25%, 89%)
+--Grayish-orange: hsl(28, 16%, 65%)
+
+#### Text
+
+--Very-dark-grayish-blue: hsl(221, 14%, 31%)
+--White: hsl(0, 0%, 100%)
+
+### Theme 2
+
+#### Backgrounds
+
+--Light-gray (main background): hsl(0, 0%, 90%)
+- Grayish red (toggle background, keypad background): hsl(0, 5%, 81%)
+- Very light gray (screen background): hsl(0, 0%, 93%)
+
+#### Keys
+
+--Dark-moderate-cyan: hsl(185, 42%, 37%)
+--Very-dark-cyan : hsl(185, 58%, 25%)
+
+--Orange: hsl(25, 98%, 40%)
+--Dark-orange: hsl(25, 99%, 27%)
+
+--Light-grayish-yellow : hsl(45, 7%, 89%)
+--Dark-grayish-orange: hsl(35, 11%, 61%)
+
+#### Text
+
+--Very-dark-grayish-yellow: hsl(60, 10%, 19%)
+- White: hsl(0, 0%, 100%)
+
+### Theme 3
+
+#### Backgrounds
+
+--Very-dark-violet: hsl(268, 75%, 9%)
+--Very-dark-violet: hsl(268, 71%, 12%)
+
+#### Keys
+
+--Dark-violet : hsl(281, 89%, 26%)
+- Vivid-magenta: hsl(285, 91%, 52%)
+
+--Pure-cyan: hsl(176, 100%, 44%)
+--Soft-cyan: hsl(177, 92%, 70%)
+
+--Very-dark-violet: hsl(268, 47%, 21%)
+--Dark-magenta: hsl(290, 70%, 36%)
+
+#### Text
+
+--Light-yellow: hsl(52, 100%, 62%)
+--Very-dark-blue: hsl(198, 20%, 13%)
+--White: hsl(0, 0%, 100%)
+
+## Typography
+
+### Body Copy
+
+- Font size (numbers): 32px
+
+### Font
+
+- Family: [League Spartan](https://fonts.google.com/specimen/League+Spartan)
+- Weights: 700
